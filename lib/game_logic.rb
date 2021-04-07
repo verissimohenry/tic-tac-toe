df3c839846fb2ee836878b13a45c47742828f4e7
verissimohenry <verissimohenry04@gmail.com>
2021-04-07 13:32:15 +0100

    add check outcome

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,4 +5,21 @@ module GameLogic
     check_outcome(player)
     game_over
   end
+
+  def check_outcome(player)
+    return unless player.answers.length >= 3
+
+    answers = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    answers.each do |sub_array|
+      intersection = sub_array & player.answers
+      if intersection == sub_array
+        @game_over = true
+        @winner = player.name
+        break
+      elsif player.counter >= 5 && intersection != sub_array
+        @game_over = true
+        @winner = 'VER'
+      end
+    end
+  end
 end
