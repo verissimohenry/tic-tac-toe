1a090d4d4e77ebfb0c1e754a06407c62ec5a455d
verissimohenry <verissimohenry04@gmail.com>
2021-04-08 12:28:09 +0100

    add test for gamelogic

--- spec/game_spec..rb
+++ spec/game_spec..rb
@@ -1,10 +1,23 @@
-# spec/calculator_spec.rb
+require './lib/game_logic'
+require './bin/main'
 
-describe Calculator do
-  describe '#add' do
-    it 'returns the sum of two numbers' do
-      calculator = Calculator.new
-      expect(calculator.add(5, 2)).to eql(7)
+describe GameLogic do
+  let(:gl) { Game.new }
+  let(:player) { Player.new }
+  let(:player2) { Player.new }
+  let(:players) { [player, player2] }
+  describe '#initialize' do
+    it 'checking setters and getters for player1 variable from Game class' do
+      gl.player1 = player
+      expect(gl.player1).to eql(player)
+    end
+    it 'checking setters and getters for player2 variable from Game class' do
+      gl.player2 = player2
+      expect(gl.player2).to eql(player2)
+    end
+    it 'checking setters and getters for players variable from Game class' do
+      gl.players = players
+      expect(gl.players).to eql(players)
     end
   end
 end
